b674c2ad24e58d66559d1c0e1c8ba85801974a34
jimmyb <jimmyb89@yahoo.com>
2020-07-13 16:30:51 -0700

    queries -GO

--- data/schema-schema-lang.js
+++ data/schema-schema-lang.js
@@ -5,42 +5,6 @@ import { resolvers } from '../resolvers';
 import { makeExecutableSchema } from 'graphql-tools';
 
 const typeDefs = `
-  enum Gender {
-    MALE
-    FEMALE 
-    OTHER
-  }
-
-  input UserInput {
-    id: ID
-    firstName: String!
-    lastName: String!
-    gender: Gender
-    createdAt: String!
-    email: String!
-    contacts: [ContactInput]
-  }
-
-  type Contact {
-    firstName: String
-    lastName: String
-  }
-
-  input ContactInput {
-    firstName: String
-    lastName: String
-  }
-
-  type User {
-    id: ID
-    firstName: String
-    lastName: String
-    gender: Gender
-    createdAt: String
-    email: String
-    contacts: [Contact]
-  }  
-
   type Vehicle{
     id: ID
     make: String
@@ -49,7 +13,6 @@ const typeDefs = `
   }
 
   input VehicleInput{
-    id: ID
     make: String
     model: String
     year: Int
@@ -57,7 +20,7 @@ const typeDefs = `
 
   type Query {
     getVehiclesByYear(year: Int!): [Vehicle]
-    getVehicleById(id: Int!): [Vehicle]
+    getVehicleById(id: String!): [Vehicle]
   }
 
   type Mutation {
@@ -67,4 +30,6 @@ const typeDefs = `
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-export default { schema };
+export { schema };
+
+
--- resolvers.js
+++ resolvers.js
@@ -1,20 +1,5 @@
-// import User from './classes/User';
 import { connection } from './db/db'
-
-/**
- * resolvers to be used with buildSchema object
- * uncomment to use with buildSchema :)
- */
-// const resolvers = {
-//   getUser: ({ root, id }) => {
-//     return new User(id, userDatabase[id]);
-//   },
-//   createUser: ({ root, input }) => {
-//     let id = require('crypto').randomBytes(10).toString('hex');
-//     userDatabase[id] = input;
-//     return new User(id, input);
-//   }
-// };
+import Vehicle from './classes/Vehicle';
 
 /**
  * Resolver map to be used with graphql-tools makeExecutableSchema.
@@ -22,8 +7,8 @@ import { connection } from './db/db'
  */
 const resolvers = {
   Query: {
-    getVehicleById: async (root, { input }) => {
-      let sql = `SELECT * FROM Vehicles WHERE id=${input.id}`;
+    getVehicleById: async (root, input) => {
+      let sql = `SELECT * FROM Vehicles WHERE id="${input.id}";`;
       const message = await new Promise((resolve, reject) => {
         connection.query(sql, (err, res) => {
           if (err) {
@@ -32,26 +17,11 @@ const resolvers = {
           resolve(res);
         });
       });
+      return message;
     },
-    getVehiclesByYear: async (root, { input }) => {
-      let sql = `SELECT * FROM Vehicles WHERE year=${input.year}`;
-      const message = await new Promise((resolve, reject) => {
-        connection.query(sql, (err, res) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(res);
-        });
-      });
-    }
-  },
-  Mutation: {
-    // createTable: async (root, { input }) => {
-    //   let sql = 'create table users(id int, firstName text, lastName text, gender text, createdAt int, email text, contacts)'
-    // },
-    createVehicle: async (root, { input }) => {
-      let id = require('crypto').randomBytes(10).toString('hex');
-      let sql = `insert into Vehicles values(${id}, ${input.make}, ${input.model}, ${input.year}`;
+    getVehiclesByYear: async (root, input) => {
+      console.log(input);
+      let sql = `SELECT * FROM Vehicles WHERE year=${input.year};`;
       const message = await new Promise((resolve, reject) => {
         connection.query(sql, (err, res) => {
           if (err) {
@@ -62,61 +32,22 @@ const resolvers = {
       });
       return message;
     }
+  },
+  Mutation: {
+    createVehicle: async (root, { input }) => {
+      let id = require('crypto').randomBytes(10).toString('hex');
+      let sql = `INSERT INTO Vehicles Values("${id}", "${input.make}", "${input.model}", ${input.year});`;
+      const message = await new Promise((resolve, reject) => {
+        connection.query(sql, (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(new Vehicle(id, input));
+        });
+      });
+      return message;
+    }
   }
 }
-
-
-// import User from './db-connection';
-
-// /**
-//  * Resolver map to be used with graphql-tools makeExecutableSchema.
-//  * Set up for persistent usage with databases
-//  */
-// const resolvers = {
-
-//   // construct all queries here
-//   Query: {
-//     getUser: (root, { id }) => {
-//       // return new User(id, userDatabase[id]);
-//     },
-//     getUsers: async (root) => {
-//       // return new User(id, userDatabase[id]);
-//       var usrs = await User.find();
-//       return usrs;
-//     },
-//   },
-
-//   Mutation: {
-//     // create a new user
-//     createUser: async (root, { input }) => {
-//       const newUser = new User({
-//         firstName: input.firstName,
-//         lastName: input.lastName,
-//         gender: input.gender,
-//         createdAt: input.createdAt,
-//         email: input.email,
-//         contacts: input.contacts,
-//       });
-//       newUser.id = newUser._id;
-
-//       // construct Promise to return and save newUser
-//       const message = await new Promise((resolve, reject) => {
-//         newUser.save((err, usr) => {
-//           if (err) {
-//             reject(err);
-//           }
-//           else {
-//             resolve(newUser);
-//           }
-//         });
-//       });
-//       return message;
-//     }
-//   }
-// }
-
-
-// export { resolvers };
-
 
 export { resolvers };
